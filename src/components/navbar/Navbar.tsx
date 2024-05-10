34c3e840da34bff4171a60f0d8866c9c7d0a6f18
Henrique_zz3 <henriquetoto3001@gmail.com>
2024-05-10 12:01:21 -0300

    CRUD Categoria

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,22 +1,20 @@
 import React from 'react'
-
+import { Link, useNavigate } from 'react-router-dom'
 
 function Navbar() {
- 
-  
 
   return (
     <>
      <div className='w-full bg-blue-900 text-white flex justify-center py-4'>
           <div className="container flex justify-between text-lg">
-            <div className='text-2xl font-bold uppercase'>Farmacia</div>
+            <Link to='/home' className='text-2xl font-bold uppercase'>Farmacia</Link>
 
             <div className='flex gap-4'>
               <div className='hover:underline'>Produto</div>
-              <div className='hover:underline'>Categoria</div>
-              <div className='hover:underline'>Cadastrar categoria</div>
-              <div className='hover:underline'>Perfil</div>
-              <div className='hover:underline'>Sair</div>
+              <Link to='/categorias' className='hover:underline'>Categoria</Link>
+              <Link to='/cadastroCategoria' className='hover:underline'>Cadastrar categoria</Link>
+              {/* <div className='hover:underline'>Perfil</div> */}
+              {/* <div className='hover:underline'>Sair</div> */}
             </div>
           </div>
         </div>
